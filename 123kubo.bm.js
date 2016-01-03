e6e94004c09e3754c0ac2514e7ce36c6e8cdff86
Arphen Lin <arphenlin@gmail.com>
2016-01-03 18:16:21 +0800

    Update 123kubo.bm.js

--- 123kubo.bm.js
+++ 123kubo.bm.js
@@ -1,29 +1,33 @@
-// javascript:(function(){var v="1.3.2";if(window.jQuery===undefined||window.jQuery.fn.jquery<v){var done=false;var script=document.createElement("script");script.src="http://ajax.googleapis.com/ajax/libs/jquery/"+v+"/jquery.min.js";script.onload=script.onreadystatechange=function(){if(!done&&(!this.readyState||this.readyState=="loaded"||this.readyState=="complete")){done=true;initMyBookmarklet();}};document.getElementsByTagName("head")[0].appendChild(script);}else{initMyBookmarklet();}function initMyBookmarklet(){$("iframe, div.header, div.menu, div.top, div.datal, div.playl, div.playr, #custom1, #stad, div.footer, div.FF tr:nth-child(1), div.FF tr:nth-child(2), object ~ *").remove();$("body").css("background-color","black");}})();
+// javascript:(function(){function getScript(url,success){var head=document.getElementsByTagName("head")[0],done=false;var script=document.createElement("script");script.src=url;script.onload=script.onreadystatechange=function(){if(!done&&(!this.readyState||this.readyState=="loaded"||this.readyState=="complete")){done=true;success();}};head.appendChild(script);console.log('test');}var v="1.3.2";if(window.jQuery===undefined||window.jQuery.fn.jquery<v){getScript("http://ajax.googleapis.com/ajax/libs/jquery/"+v+"/jquery.min.js",initMyBookmarklet);}else{initMyBookmarklet();}function initMyBookmarklet(){$("iframe, div.header, div.menu, div.top, div.datal, div.playl, div.playr, #custom1, #stad, div.footer, div.FF tr:nth-child(1), div.FF tr:nth-child(2), object ~ *").remove();$("body").css("background-color","black");}})();
 
-(function(){
+(function() {
+    function getScript(url, success){
+        var head = document.getElementsByTagName("head")[0], done = false;
+        var script = document.createElement("script");
+        script.src = url;
 
-	// the minimum version of jQuery we want
-	var v = "1.3.2";
+        // Attach handlers for all browsers
+        script.onload = script.onreadystatechange = function(){
+            if ( !done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete") ) {
+                done = true;
+                success();
+            }
+        };
 
-	// check prior inclusion and version
-	if (window.jQuery === undefined || window.jQuery.fn.jquery < v) {
-		var done = false;
-		var script = document.createElement("script");
-		script.src = "http://ajax.googleapis.com/ajax/libs/jquery/" + v + "/jquery.min.js";
-		script.onload = script.onreadystatechange = function(){
-			if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
-				done = true;
-				initMyBookmarklet();
-			}
-		};
-		document.getElementsByTagName("head")[0].appendChild(script);
-	} else {
-		initMyBookmarklet();
-	}
-	
-	function initMyBookmarklet() {
-		$("iframe, div.header, div.menu, div.top, div.datal, div.playl, div.playr, #custom1, #stad, div.footer, div.FF tr:nth-child(1), div.FF tr:nth-child(2), object ~ *").remove();
-		$("body").css("background-color", "black");
-	}
+        head.appendChild(script);
+    }
+
+    // check prior inclusion and version
+    var v = "1.3.2";
+    if (window.jQuery === undefined || window.jQuery.fn.jquery < v) {
+        getScript("http://ajax.googleapis.com/ajax/libs/jquery/" + v + "/jquery.min.js", initMyBookmarklet);
+    } else {
+        initMyBookmarklet();
+    }
+
+    function initMyBookmarklet() {
+        $("iframe, div.header, div.menu, div.top, div.datal, div.playl, div.playr, #custom1, #stad, div.footer, div.FF tr:nth-child(1), div.FF tr:nth-child(2), object ~ *").remove();
+        $("body").css("background-color", "black");
+    }
 
 })();
